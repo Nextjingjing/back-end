78d956fdda750e929b8c927f490d77bf21f55631
SNext <snext2004@gmail.com>
2024-09-06 18:00:16 +0700

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+// 3rd Party Modules 
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const connectDb = require('./config/db');
+require('dotenv/config'); 
+
+// Local Modules 
+const myRoute = require('./routes/myRoute'); 
+
+// Server Initialization 
+const app = express(); 
+const PORT = process.env.PORT; 
+
+// Middlewares 
+app.use(express.json()); 
+
+// Routes will be written here 
+app.use('/route', myRoute);
+
+
+// Server Listen Along with Database 
+// connection(in case of data persistence) 
+app.listen(PORT, async (error) =>{
+
+	await connectDb();
+	if(!error) 
+		console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
+	else
+		console.log("Error occurred, server can't start", error); 
+	} 
+);
